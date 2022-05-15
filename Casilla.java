7120a0b75da7929c4388881239f178512f8193d9
Dsanabria02 <100260303+Dsanabria02@users.noreply.github.com>
2022-05-14 22:23:23 -0600

    Add files via upload

---
+++ Casilla.java
@@ -0,0 +1,15 @@
+
+package proyecto.pkg2;
+
+/**
+ *
+ * @author Diana
+ */
+public class Casilla {
+    private int posX;
+    private int posY;
+    
+    public Casilla (){
+        int colorear;
+    }
+}
