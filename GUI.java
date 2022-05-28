ed81a2750fb7aabcb16aeaa4a900c45bd69699cb
REVAN <joseaguero11@estudiantec.cr>
2022-05-28 11:42:29 -0600

    Update tablero y dirección de movimiento

--- GUI.java
+++ GUI.java
@@ -3,6 +3,9 @@ import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -14,7 +17,7 @@ public class GUI extends JFrame implements ActionListener, Constantes, KeyListen
     Dot dot;
     Personaje pj;
 
-    /*private static Teclado teclado;
+    /*private static Teclado teclado; // Flags
 
     private final static int numeroTeclas = 120;
     private final boolean[] teclas = new boolean[numeroTeclas];
@@ -24,73 +27,45 @@ public class GUI extends JFrame implements ActionListener, Constantes, KeyListen
     public boolean izquierda;
     public boolean derecha; */
     GUI(){
+        this.setTitle("Proyecto 2 - Roguelike");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
         this.addKeyListener(this);
-        agregarElementos();
-        this.pack();
+        this.setBounds(50,50,740,745); // Cambiar para los nuevos paneles
+        ordenaComplementos();
+        crearTablero(1, 0, 0);
+        
+        //agregarElementos(); // Flags
+        //this.pack();
     }
     
-    public void agregarElementos(){
-        //teclado = new Teclado();
-        //teclado.addKeyListener(this);
-        //addKeyListener(teclado);
+    private void crearTablero(int opcion, int x, int y){
+        //this.setBounds(50,50,1145,800);
+        JPanel pn = new JPanel(){
+            @Override
+            public void paint(Graphics g) {
+                super.paint(g);
+                for(int y = 0; y < 20; y++){
+                    for(int x = 0; x < 20; x++){
+                        g.setColor(Color.black);
+                        g.drawRect(x*35, y*35, 35, 35);
+                    }
+                }
+                pj.paintPersonaje(g);
+            }
+        };
+        this.add(pn, BorderLayout.CENTER);
+    }
 
-        
-
-        next = new JButton("continuar");
-        next.addActionListener(this);
-        next.setActionCommand("next");
-
-        mapa = new Mapa(this);
-
-        //ventana = new JFrame();
-        this.add(mapa.panelTablero);
-        //this.add(next, BorderLayout.SOUTH); // Para botón
-        //ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //ventana.pack();
-        //ventana.setVisible(true);
-        //ventana.addKeyListener(this);
-
-        dot = new Dot();
-        moveDot();
-
+    public void ordenaComplementos(){
         pj = new Personaje();
-
-        //accionTeclado();
-        //ventana.addKeyListener(ventana);
-
+        //pj.posicion[X] = 0; // Flags
+        //pj.posicion[Y] = 0;
     }
 
     /**Este metodo se ejecuta cuando se presiona una tecla*/
     @Override
     public void keyPressed(KeyEvent e) {
-        
-        /*if (e.VK_W == e.getKeyCode())
-        {
-            System.out.println("Arriba");
-        }
-        /*if (e.VK_E==e.getKeyCode())
-        {
-            vocales+="e "; 
-            cantidadVocales++;
-        }
-        if (e.VK_I==e.getKeyCode())
-        {
-            vocales+="i ";
-            cantidadVocales++;
-        }
-        if (e.VK_O==e.getKeyCode())
-        {
-            vocales+="o "; 
-            cantidadVocales++;
-        }
-        if (e.VK_U==e.getKeyCode())
-        {
-            vocales+="u ";
-            cantidadVocales++;
-        } */
-        
     }
 
     @Override
@@ -103,62 +78,34 @@ public class GUI extends JFrame implements ActionListener, Constantes, KeyListen
         if (e.VK_W == e.getKeyCode())
         {
             System.out.println("W presionada");
-            dot.move(1);
-            moveDot();
+            pj.move(4);
+            pj.dir = 1;
+            this.repaint();
         }
         if (e.VK_D == e.getKeyCode())
         {
             System.out.println("D presionada");
-            dot.move(2);
-            moveDot();
+            pj.move(3);
+            pj.dir = 2;
+            this.repaint();
         }
         if (e.VK_S == e.getKeyCode())
         {
             System.out.println("S presionada");
-            dot.move(3);
-            moveDot();
+            pj.move(2);
+            pj.dir = 3;
+            this.repaint();
         }
         if (e.VK_A == e.getKeyCode())
         {
             System.out.println("A presionada");
-            dot.move(4);
-            moveDot();
+            //dot.move(4); // Flag
+            //moveDot();
+            pj.move(1);
+            pj.dir = 4;
+            this.repaint();
         }
-        //System.out.println("You released key char: " + e.getKeyChar());
-        
     }
-
-    /*public void actualizar() {
-        arriba = teclas[KeyEvent.VK_W];
-        abajo =  teclas[KeyEvent.VK_S];
-        izquierda =  teclas[KeyEvent.VK_A];
-        derecha = teclas[KeyEvent.VK_D];
-    }
-
-    protected void accionTeclado(){
-        KeyListener l = new KeyListener(){
-
-            @Override
-            public void keyTyped(KeyEvent e) {
-                teclas[e.getKeyCode()] = true;
-            }
-        
-            @Override
-            public void keyPressed(KeyEvent e) {
-                teclas[e.getKeyCode()] = false;
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-                
-            }
-
-        };
-        ventana.addKeyListener(l);
-        if(teclado.arriba){
-            System.out.println("Arriba");
-        }
-    } */
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -173,22 +120,6 @@ public class GUI extends JFrame implements ActionListener, Constantes, KeyListen
             ((Casilla)e.getSource()).setAsTarget();
             moveDot();
         }
-
-        /* teclado.actualizar();
-
-        if(teclado.arriba){
-            System.out.println("Arriba");
-        }
-        if(teclado.abajo){
-            System.out.println("Abajo");
-        }
-        if(teclado.izquierda){
-            System.out.println("Izquierda");
-        }
-        if(teclado.derecha){
-            System.out.println("Derecha");
-        } */
-        
     }
 
     public void moveDot(){
--- Personaje.java
+++ Personaje.java
@@ -1,13 +1,62 @@
+import java.awt.Graphics;
+import java.awt.Color;
+
 public class Personaje implements Constantes{
     int[] posicion;
+    public int posX;
+    public int posY;
+    public int dir;
 
     public Personaje(){
         posicion = new int[2];
-        posicion[X] = 0;
-        posicion[Y] = 0;
+        posX = 1;
+        posY = 1;
+        dir = 1;
     }
 
-    public void move(){
-        posicion[X] += 1;
+    public void paintPersonaje(Graphics g){
+        if (dir == 1){
+            g.setColor(Color.RED);
+            g.fillRect(posX, posY, 34, 34);
+
+            g.setColor(Color.YELLOW);
+            g.fillRect(posX, posY, 34, 8);
+        }
+        if (dir == 2){
+            g.setColor(Color.RED);
+            g.fillRect(posX, posY, 34, 34);
+
+            g.setColor(Color.YELLOW);
+            g.fillRect(posX+26, posY, 8, 34);
+        }
+        if (dir == 3){
+            g.setColor(Color.RED);
+            g.fillRect(posX, posY, 34, 34);
+
+            g.setColor(Color.YELLOW);
+            g.fillRect(posX, posY+26, 34, 8);
+        }
+        if (dir == 4){
+            g.setColor(Color.RED);
+            g.fillRect(posX, posY, 34, 34);
+
+            g.setColor(Color.YELLOW);
+            g.fillRect(posX, posY, 8, 34);
+        }
+    }
+
+    public void move(int iden){
+        if (iden == 1 && posX > 1){
+            posX -= 35; 
+        }
+        if (iden == 2 && posY < 666){
+            posY += 35; 
+        }
+        if (iden == 3 && posX < 666){
+            posX += 35; 
+        }
+        if (iden == 4 && posY > 1){
+            posY -= 35; 
+        }
     }
 }
