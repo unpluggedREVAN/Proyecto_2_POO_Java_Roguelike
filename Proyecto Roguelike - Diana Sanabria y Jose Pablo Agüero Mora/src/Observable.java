44f2fe382473f594387496720c5ff300aae26221
REVAN <joseaguero11@estudiantec.cr>
2022-06-04 16:18:06 -0600

    Observer added

---
+++ Proyecto Roguelike - Diana Sanabria y Jose Pablo Agüero Mora/src/Observable.java
@@ -0,0 +1,8 @@
+// Proyecto roguelike - POO
+// Diana Sanabria (2021436548) / Jose Pablo Agüero Mora (2021126372) 
+// Interfaz Observable
+
+public interface Observable {
+    public void notificar();
+    public void eliminarObserver();
+}
