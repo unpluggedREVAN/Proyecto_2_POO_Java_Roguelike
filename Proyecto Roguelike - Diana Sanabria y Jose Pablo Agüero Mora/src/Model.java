575dd4c1fba02a6762906b206a9bbb8ea58b623f
REVAN <joseaguero11@estudiantec.cr>
2022-06-04 16:17:44 -0600

    MVC (Model)

---
+++ Proyecto Roguelike - Diana Sanabria y Jose Pablo Agüero Mora/src/Model.java
@@ -0,0 +1,26 @@
+// Proyecto roguelike - POO
+// Diana Sanabria (2021436548) / Jose Pablo Agüero Mora (2021126372) 
+// Interfaz Model
+
+import java.awt.Color;
+
+public interface Model {
+    
+    public static final int CASILLA_WIDTH = 35;
+    public static final int CASILLA_HEIGHT = 35;
+    public static final int TABLERO_SIZE = 20;
+
+    public static final int TURNOS_RESPAWN = 10;
+    public static final int LIMITE_COMPLEMENTOS = 3;
+
+    public static final Color PJ_COLOR_1 = Color.RED;
+    public static final Color PJ_COLOR_2 = Color.YELLOW;
+
+    public static final Color ALIADO_COLOR = Color.BLUE;
+    public static final Color ENEMIGO_COLOR = Color.GREEN;
+    
+    public static final int LIMITE_HORIZONTAL = 666;
+    public static final int VIDA_DEFAULT = 8;
+
+    public static final int POSIBILIDADES = 19;
+}
