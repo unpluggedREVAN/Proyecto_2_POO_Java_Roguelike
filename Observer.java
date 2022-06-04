6ac8fd3df717709f24df4c410703e809c287b16f
REVAN <joseaguero11@estudiantec.cr>
2022-06-04 16:06:18 -0600

    Update Observer.java

--- Observer.java
+++ Observer.java
@@ -1,3 +1,7 @@
+// Proyecto roguelike - POO
+// Diana Sanabria (2021436548) / Jose Pablo Agüero Mora (2021126372) 
+// Interfaz Observer
+
 public interface Observer {
     public void update(int posX, int posY);
 }
