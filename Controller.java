7366322492ca6edfcb2f351e8126c1918b340141
REVAN <joseaguero11@estudiantec.cr>
2022-06-04 16:05:54 -0600

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,3 +1,7 @@
+// Proyecto roguelike - POO
+// Diana Sanabria (2021436548) / Jose Pablo Agüero Mora (2021126372) 
+// Clase Controller
+
 public class Controller implements Model{
 
     Factory creador = new Factory();
